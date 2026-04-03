984f250b52fdf970225d7f794d53c0b19a74a1d8
Cristiano <cristianoatmcrs@gmail.com>
2026-04-02 21:26:05 -0300

    Corrigindo incompatibilidade de atributos entre as conversões.

--- src/main/java/br/com/municipio/vacinas/vacinas_api/mapper/LocalMapper.java
+++ src/main/java/br/com/municipio/vacinas/vacinas_api/mapper/LocalMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-
 import br.com.municipio.vacinas.vacinas_api.model.LocalVacina;
 import br.com.municipio.vacinas.vacinas_api.dto.LocalRequestDTO;
 import br.com.municipio.vacinas.vacinas_api.dto.LocalResponseDTO;
@@ -18,8 +17,23 @@ public interface LocalMapper {
 
     LocalVacina toLocalVacina(LocalResponseDTO dto);
 
+    @Mapping(target = "rua", ignore = true)
+    @Mapping(target = "numero", ignore = true)
+    @Mapping(target = "bairro", ignore = true)
+    @Mapping(target = "cep", ignore = true)
+    @Mapping(target = "cidade", ignore = true)
+    @Mapping(target = "estado", ignore = true)
+    @Mapping(target = "id", ignore = true)
     LocalResponseDTO toDTO(LocalVacina entity);
-
+    
+    @Mapping(target = "rua", ignore = true)
+    @Mapping(target = "bairro", ignore = true)
+    @Mapping(target = "numero", ignore = true)
+    @Mapping(target = "cep", ignore = true)
+    @Mapping(target = "cidade", ignore = true)
+    @Mapping(target = "estado", ignore = true)
     List<LocalResponseDTO> toDTOList(List<LocalVacina> entities);
+
+
 
 }
