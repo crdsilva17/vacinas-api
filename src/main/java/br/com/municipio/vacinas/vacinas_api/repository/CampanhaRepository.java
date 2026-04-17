22295b94da7eeaa3f0bfc5996acd9c786e519d58
Cristiano <cristianoatmcrs@gmail.com>
2026-04-16 21:49:36 -0300

    Criando CampanhaRepository

---
+++ src/main/java/br/com/municipio/vacinas/vacinas_api/repository/CampanhaRepository.java
@@ -0,0 +1,7 @@
+package br.com.municipio.vacinas.vacinas_api.repository;
+
+import br.com.municipio.vacinas.vacinas_api.model.CampanhaVacinacao;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CampanhaRepository extends MongoRepository<CampanhaVacinacao, String> {
+}
