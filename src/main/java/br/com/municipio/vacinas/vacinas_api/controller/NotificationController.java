5e683e1c76812cf9b62320150e54d90d957e8c7c
Cristiano <cristianoatmcrs@gmail.com>
2026-06-07 23:23:14 -0300

    fixed bud

--- src/main/java/br/com/municipio/vacinas/vacinas_api/controller/NotificationController.java
+++ src/main/java/br/com/municipio/vacinas/vacinas_api/controller/NotificationController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.municipio.vacinas.vacinas_api.repository.NotificationRepository;
+import br.com.municipio.vacinas.vacinas_api.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
@@ -12,13 +13,17 @@ import br.com.municipio.vacinas.vacinas_api.repository.NotificationRepository;
 public class NotificationController {
 
     private final NotificationRepository notificationRepository;
+    private final UsuarioRepository usuarioRepository;
 
     @GetMapping("/count")
     public long count(Authentication authentication) {
 
         return notificationRepository
                 .countByUserIdAndReadFalse(
-                        authentication.getName()
+                        usuarioRepository
+                                .findUserByEmail(authentication.getName())
+                                .orElseThrow()
+                                .getId()
                 );
     }
 }
