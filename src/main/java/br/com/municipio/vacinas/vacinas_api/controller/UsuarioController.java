0579b726b7edf68613f788ec5cdf7e9646aac908
Cristiano <cristianoatmcrs@gmail.com>
2026-06-08 14:46:49 -0300

    fixes

--- src/main/java/br/com/municipio/vacinas/vacinas_api/controller/UsuarioController.java
+++ src/main/java/br/com/municipio/vacinas/vacinas_api/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +28,7 @@ public class UsuarioController {
     }
 
     @PutMapping
-    public ResponseEntity<UsuarioResponseDTO> updateUser(UserRequestDTO request) {
+    public ResponseEntity<UsuarioResponseDTO> updateUser(@RequestBody UserRequestDTO request) {
         System.out.println("Atualizando usuário: " + request);
         System.out.println(request.email());
         return ResponseEntity.ok(service.updateUserByEmail(request));
